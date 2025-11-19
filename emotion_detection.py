a50b05f536438231de381abad97705ec27d78e2f
Badal Chhipa <badalchhipa9@gmail.com>
2025-11-19 04:09:18 -0500

    Enhance emotion detector to output relevent properties

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-    import requests
+import requests
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +6,9 @@ def emotion_detector(text_to_analyze):
     payload = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json = payload, headers = header)
+    emotions = response.json()['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
 
-    return response.text
+    return emotions
